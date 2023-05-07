2dece06a1255241f76bf332d08210d7b89cb5380
Tayo Adeyeye <tn.adeyeye@gmail.com>
2023-05-07 11:47:14 -0400

    train modification for output

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@
 #SBATCH --qos=default
 #SBATCH --account=class
 #SBATCH --gres=gpu:1
-#SBATCH --output=slurm_output/slurm-%j.out
-#SBATCH --error=slurm_output/slurm-%j.err
+#SBATCH --output=/fs/classhomes/spring2023/cmsc828a/c828a006/slurm_output/slurm-%j.out
+#SBATCH --error=/fs/classhomes/spring2023/cmsc828a/c828a006/slurm_output/slurm-%j.err
 set -x
 
 srun bash -c "source /fs/classhomes/spring2023/cmsc828a/c828a050/.profile;conda activate cmsc828_final; \
