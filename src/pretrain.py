c6dab12ecf4d0a6de9ff59d35d87800c4ba588d1
Tayo Adeyeye <tn.adeyeye@gmail.com>
2023-05-08 00:12:32 -0400

    modification to inception module for mtl

--- src/pretrain.py
+++ src/pretrain.py
@@ -1,6 +1,6 @@
 import pathlib, datetime
 from dataloader import PrepareDataLoader
-from model import inception, inception_cl, KD_loss
+from model import inception, inception_cl, inception_mtl, KD_loss
 from utils.utils import get_logger
 import numpy as np
 from tensorflow import keras
@@ -131,9 +131,34 @@ class Trainer:
 
 
     
-    # TODO train multitask learning
+    # TODO train multitask learning  Temporry using baseling to test
     def train_mtl(self):
-        raise NotImplementedError
+        batch_size = self.experiment_config["batch_size"]
+        nb_epochs = self.experiment_config["training_epochs"]
+        verbose = self.experiment_config["verbose"]
+        # load data
+        dataloader = PrepareDataLoader(self.pretrain_config, self.experiment_config)
+        self.logger.info(f"Loading data ...")
+        train_dataloader, valid_dataloader  = dataloader.load_pretrain_data(labels = self.universal_label, model_type = "baseline")
+        if not self.output_dir.is_dir():
+            self.logger.warning(f"Parent directory {self.output_dir} not found. Creating directory")
+            self.output_dir.mkdir(parents=True, exist_ok=True)
+        nb_classes = len(self.universal_label)
+
+        input_shape =(3,1)
+        ## DO NOT SET BATCH SIZE HERE
+        model = inception_mtl.Classifier_INCEPTION(self.output_dir, input_shape, nb_classes,
+                                                                verbose=verbose, 
+                                                                build=True, 
+                                                                nb_epochs = nb_epochs,
+                                                                use_bottleneck = False
+                                                                )
+        self.logger.info("---- Start training ----") 
+        model.fit(train_dataloader, valid_dataloader)
+        self.logger.info("---- End training ----")
+        self.logger.info("---- Start validation----") 
+        model.predict(train_dataloader, valid_dataloader)
+        self.logger.info("---- End validation ----")
 
 
 
