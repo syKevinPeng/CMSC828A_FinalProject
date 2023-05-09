86231f03daa7628b6a855f4a6388d865dc4a1d93
Siyuan Peng <c828a050@nexusclass00.umiacs.umd.edu>
2023-05-09 18:42:45 -0400

    remove redundent file

--- train.sh
+++ train.sh
@@ -4,12 +4,12 @@
 #SBATCH --qos=default
 #SBATCH --account=class
 #SBATCH --gres=gpu:1
-#SBATCH --output=/fs/classhomes/spring2023/cmsc828a/c828a006/slurm_output/slurm-%j.out
-#SBATCH --error=/fs/classhomes/spring2023/cmsc828a/c828a006/slurm_output/slurm-%j.err
+#SBATCH --output=slurm_output/slurm-%j.out
+#SBATCH --error=slurm_output/slurm-%j.err
 set -x
 
 srun bash -c "source /fs/classhomes/spring2023/cmsc828a/c828a050/.profile;\
 python3 src/main.py \
-    --config src/configs/config_tayo.yml \
+    --config src/configs/config_siyuan.yml \
     --train"
  
