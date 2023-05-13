45cf33dac40fc38debc10a158b92911ba5c5e524
Lining Wang <lining.somewhere@gmail.com>
2023-05-13 16:58:54 -0400

    update train

--- train.sh
+++ train.sh
@@ -5,12 +5,12 @@
 #SBATCH --account=class
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
-#SBATCH --output=/fs/classhomes/spring2023/cmsc828a/c828a010/CMSC828A_FinalProject/slurm_output/slurm-%j.out
-#SBATCH --error=/fs/classhomes/spring2023/cmsc828a/c828a010/CMSC828A_FinalProject/slurm_output/slurm-%j.err
+#SBATCH --output=/fs/classhomes/spring2023/cmsc828a/<your directory>/CMSC828A_FinalProject/slurm_output/slurm-%j.out
+#SBATCH --error=/fs/classhomes/spring2023/cmsc828a/<your directory>/CMSC828A_FinalProject/slurm_output/slurm-%j.err
 set -x
 
-srun bash -c "source /fs/classhomes/spring2023/cmsc828a/c828a010/.profile;\
+srun bash -c "source /fs/classhomes/spring2023/cmsc828a/<your directory>/.profile;\
 python3 src/main.py \
-    --config src/configs/config_lining.yml \
+    --config src/configs/<your config>\
     --train"
  
